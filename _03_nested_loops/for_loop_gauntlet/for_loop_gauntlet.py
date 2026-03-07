2547c5bc056e332c0c786246f673674f44128e89
Matthew Seul <132800763+SigmaBlobfish69@users.noreply.github.com>
2026-03-06 17:03:39 -0800

    Update for_loop_gauntlet.py

--- _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
+++ _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
@@ -5,4 +5,33 @@ https://central.jointheleague.org/levels/Level0/Recipes/ForLoopGauntlet.html
 
 
 if __name__ == '__main__':
-    pass
+
+    for i in range(101, 0, -1):
+        print(i)
+
+    for i in range(0, 101, 2):
+        print(i)
+
+    for i in range(1, 99, 2):
+        print(i)
+
+    for i in range(1, 501, 1):
+        if i % 2 == 1:
+            print(str(i) + " is odd")
+        if i % 2 == 0:
+            print(str(i) + " is even")
+    for i in range(0, 778, 7):
+        print(i)
+    birth_year = 2012
+    current_year = 2026
+
+    for year in range(birth_year, current_year + 1):
+        print("In " + str(year) + ", I was " + str(year-birth_year) + " years old.")
+
+    for i in range(3):
+        for n in range(3):
+            print(str(i) + " " + str(n))
+
+    for i in range(1, 10):
+       for i in range():
+           print(i, end=)
