623f383ca2bd4a16d24dd1ee4fec6c5d0dc93091
league <ggg>
2026-04-03 16:56:02 -0700

    I worked on loops and stuffies

--- _01_function_writing/_d_happy_pet.py
+++ _01_function_writing/_d_happy_pet.py
@@ -20,7 +20,16 @@ def feedPet(pet):
     global happiness_level
     messagebox.showinfo(title="user", message=f"Your {pet} is very happy about getting fed!")
     happiness_level += 20
-
+def killPet(pet):
+    global happiness_level
+    method = simpledialog.askstring(title="user", prompt=f"What method to kill your {pet}? Nuke, pistol, or knife?")
+    if method == "nuke":
+        messagebox.showinfo(title="user", message=f"Your {pet} has been disintegrated into nothing but ash! Also your house has been blown to bits and you died, too. BAD CHOICE, BUDDY!")
+    if method == "pistol":
+        messagebox.showinfo(title="user", message=f"The impact of the bullet from your pistol was so forceful that if blew off your {pet}'s head!")
+    if method == "knife":
+        messagebox.showinfo(title="user", message=f"You have slit your dog's throat, and now it is gurgling on the floor like a pathetic weakling!")
+    happiness_level -=100
 if __name__== '__main__' :
     # TODO)
     #   1. Ask the user to enter the type of pet they want (give them a few
@@ -28,7 +37,7 @@ if __name__== '__main__' :
     pet = simpledialog.askstring(title="user", prompt="What pet do you want? Dog, cat, fish, guinea pig, horse, donkey?")
 
     while True:
-        activity = simpledialog.askstring(title="user", prompt=f"What activity would you like your pet to do? Currently your pet is {happiness_level}/100")
+        activity = simpledialog.askstring(title="user", prompt=f"What activity would you like your {pet} to do? Currently your pet is {happiness_level}/100")
         if activity == None or activity == '':
             exit()
         if activity == "walk":
@@ -36,6 +45,9 @@ if __name__== '__main__' :
 
         if activity == "feed":
             feedPet(pet)
+
+        if activity == "kill":
+            killPet(pet)
 
 
     #   2. Use a loop (maybe a while loop?) to keep offering interactions with
@@ -46,7 +58,8 @@ if __name__== '__main__' :
     #      level by a different amount, depending on the kind of pet they
     #      have. For example, a fish might not enjoy a walk!
         if happiness_level >= 100:
-            messagebox.showinfo(title="user", message=f"Your {pet} has reached the max happiness level!")
+            messagebox.showinfo(title="user", message=f"Your {pet} has reached the max happiness level! Good job!")
             exit()
         if happiness_level <= 0:
-            messagebox.showinfo(title="user", message=f"Your {pet} is very sad and will never do activities with you ever again!")
+            messagebox.showinfo(title="user", message=f"Your {pet} is dead because you did not make it happy (also because you killed it). Go jump off a cliff, loser.")
+            exit()
--- _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
+++ _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
@@ -5,33 +5,17 @@ https://central.jointheleague.org/levels/Level0/Recipes/ForLoopGauntlet.html
 
 
 if __name__ == '__main__':
-
-    for i in range(101, 0, -1):
-        print(i)
-
-    for i in range(0, 101, 2):
-        print(i)
-
-    for i in range(1, 99, 2):
-        print(i)
-
-    for i in range(1, 501, 1):
-        if i % 2 == 1:
-            print(str(i) + " is odd")
-        if i % 2 == 0:
-            print(str(i) + " is even")
-    for i in range(0, 778, 7):
-        print(i)
-    birth_year = 2012
-    current_year = 2026
-
-    for year in range(birth_year, current_year + 1):
-        print("In " + str(year) + ", I was " + str(year-birth_year) + " years old.")
-
     for i in range(3):
-        for n in range(3):
-            print(str(i) + " " + str(n))
+        for j in range(1, 4):
+            print(j+3*i, end=" ")
+        print( )
 
-    for i in range(1, 10):
-       for i in range():
-           print(i, end=)
+    for i in range(10):
+        for j in range(1, 11):
+            print(j+10*i, end=" ")
+        print( )
+
+    for i in range(6):
+        for j in range(1*i):
+            print("*", end=" ")
+        print( )
